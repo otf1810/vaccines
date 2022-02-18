86b7b97c847f587412973324d10d345022ab76e3
outf <1441281543@qq.com>
2022-02-18 23:51:37 +0800

    首页资讯栏

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TouchableHighlight} from 'react-native';
+import { ScrollView, TouchableHighlight, TouchableOpacity } from 'react-native';
 import {
   Dimensions,
   Image,
@@ -9,49 +9,72 @@ import {
   Button,
   Text,
 } from 'react-native';
-import {Divider, ListItem, SearchBar} from 'react-native-elements';
-import {HomeStyle as styles} from '../../../styles/Style';
+import { Card, Divider, ListItem, SearchBar } from 'react-native-elements';
+import { HomeStyle as styles } from '../../../styles/Style';
 
 const screen = Dimensions.get('screen');
 
 export default Home = () => {
   return (
-    <View style={{backgroundColor: 'white'}}>
+    <ScrollView contentContainerStyle={styles.page} showsVerticalScrollIndicator={false}>
+
       <Image
         style={styles.banner}
-        source={require('../../assets/home_banner.jpeg')}
+        source={require('../../assets/home/home_banner.jpeg')}
       />
-      <Text style={styles.newsHeader}>最新资讯</Text>
-      <ListItem
-        Component={TouchableHighlight}
-        containerStyle={{}}
-        disabledStyle={{opacity: 0.5}}
-        onLongPress={() => console.log('onLongPress()')}
-        onPress={() => console.log('onLongPress()')}
-        pad={20}
-        bottomDivider
-        topDivider>
-        <ListItem.Content>
-          <ListItem.Title>
-            <Text>第四金!速度滑冰男子500米</Text>
-          </ListItem.Title>
-          {/* <ListItem.Subtitle>
-            <Text>React Native Elements</Text>
-          </ListItem.Subtitle> */}
-        </ListItem.Content>
-        <Text>{'>'}</Text>
-      </ListItem>
-      <ListItem bottomDivider>
-        <ListItem.Content>
-          <ListItem.Title>
-            <Text>第四金!速度滑冰男子500米</Text>
-          </ListItem.Title>
-          {/* <ListItem.Subtitle>
-            <Text>React Native Elements</Text>
-          </ListItem.Subtitle> */}
-        </ListItem.Content>
-        <Text>{'>'}</Text>
-      </ListItem>
-    </View>
+
+      {/* <Text style={styles.newsHeader}>最新资讯</Text>
+        <ListItem
+          Component={TouchableHighlight}
+          containerStyle={{borderRadius: 0.5}}
+          disabledStyle={{ opacity: 0.5 }}
+          onLongPress={() => console.log('onLongPress()')}
+          onPress={() => console.log('onLongPress()')}
+          pad={20}
+          bottomDivider
+          topDivider>
+          <ListItem.Content>
+            <ListItem.Title>
+              <Text>第四金!速度滑冰男子500米</Text>
+            </ListItem.Title>
+          </ListItem.Content>
+          <Text>{'>'}</Text>
+        </ListItem> */}
+
+      <View style={styles.newsBlock}>
+        <View style={styles.newsHeader}>
+          <Text>最新资讯</Text>
+          <TouchableOpacity>
+            <Image style={styles.more} source={require('../../assets/home/arrow.png')}></Image>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.newsItemList}>
+          <TouchableOpacity style={styles.newsItem}>
+            <Text style={styles.newsTitle}>华为大众“牵手”疑云：合资生</Text>
+            <Text style={styles.newsContent}>据媒体2月17日报道，大众汽车集团计划以数。</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.newsItem}>
+            <Text style={styles.newsTitle}>华为大众“牵手”疑云：合资生</Text>
+            <Text style={styles.newsContent}>据媒体2月17日报道，大众汽车集团计划以数。</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.newsItem}>
+            <Text style={styles.newsTitle}>华为大众“牵手”疑云：合资生</Text>
+            <Text style={styles.newsContent}>据媒体2月17日报道，大众汽车集团计划以数。</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+
+      {/* <TouchableOpacity>
+          <Card containerStyle={{borderRadius: 0.8}} wrapperStyle={{borderRadius:0.8}}>
+            <Card.Title>布偶猫</Card.Title>
+            <Card.Image
+              source={require('../../assets/cat.jpg')}
+            />
+          </Card>
+        </TouchableOpacity> */}
+
+    </ScrollView>
+
   );
 };
--- styles/Style.js
+++ styles/Style.js
@@ -4,17 +4,59 @@ import { Dimensions, StyleSheet } from 'react-native';
 const screen = Dimensions.get('screen');
 
 const HomeStyle = StyleSheet.create({
+  page: {
+    alignItems: 'center' 
+  },
   banner: {
-    width: screen.width,
+    width: 360,
     height: screen.height / 5,
+    borderRadius: 20,
+    marginTop: 20
+  },
+  newsBlock: {
+    height: 260,
+    width: 370,
+    // borderWidth: 1,
+    alignItems: 'center'
   },
   newsHeader: {
-    fontWeight: 'bold',
-    fontSize: 16,
-    marginLeft: 5,
-    marginTop: 10,
+    width: 350,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    // alignSelf: 'stretch',
+    // padding: 15,
+    paddingVertical: 15,
+    // borderWidth: 1
+  },
+  more: {
+    width: 25,
+    height: 25,
+  },
+  newsItemList: {
+    width: 350,
+    // borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  newsItem: {
+    // height: 75,
+    width: 345,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    shadowColor: 'gray',
+    shadowOffset: 5,
+    padding: 8,
     marginBottom: 10,
+  },
+  newsTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  newsContent: {
+    fontSize: 14,
   }
+  
 });
 
 export {
